5a5c36821b1589d749734c81032284b1e69fac1b
Alex Lee <hyunho98@gmail.com>
2020-01-27 00:53:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'Male', 'Black', 'Rowdy', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 12, 'Female', 'Black', 'Mellow', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sargeant Brown', 22, 'Male', 'Brown', 'Stern', false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 4, 'Female', 'Brown', 'Relaxed', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 14, 'Male', 'Black', 'Grumpy', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 15, 'Female', 'Black', 'Feisty', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 17, 'Male', 'Black', 'Aggressive', true);
