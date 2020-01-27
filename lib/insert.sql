ee97e678a043b31b8b72ecae44a69af48c219a3e
Alex Lee <hyunho98@gmail.com>
2020-01-27 00:49:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 19, 'Male', 'Brown', 'Angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 19, 'Male', 'Brown', 'Happy', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'Male', 'Black', 'Rowdy', true)
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ()
